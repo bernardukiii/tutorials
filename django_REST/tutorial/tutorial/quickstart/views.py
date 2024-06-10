aed2dafb02ccd344d96cec73fe1cdb43fbf6d701
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-06-10 12:05:24 -0300

    chore: adding both viewsets

--- django_REST/tutorial/tutorial/quickstart/views.py
+++ django_REST/tutorial/tutorial/quickstart/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+
+from tutorial.quickstart.serializers import GroupSerializer, UserSerializer
 
 # Create your views here.
+    # Groups of views are called VIEWSETS
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
