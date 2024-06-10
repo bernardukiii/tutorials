9ab7ea91064810293d456b494d130dd905b1d498
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-06-10 12:31:40 -0300

    fix: replacing user with group

--- django_REST/tutorial/tutorial/quickstart/views.py
+++ django_REST/tutorial/tutorial/quickstart/views.py
@@ -12,6 +12,6 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 class GroupViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('name')
+    queryset = Group.objects.all().order_by('name')
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
