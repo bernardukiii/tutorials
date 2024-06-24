b0178690610d43045df06095f213d090becb009c
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-06-24 11:01:45 -0300

    fix: adding missing field

--- drf-serialization/tutorial/snippets/serializers.py
+++ drf-serialization/tutorial/snippets/serializers.py
@@ -8,7 +8,7 @@ class SnippetSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner', 'highlight']
     
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
